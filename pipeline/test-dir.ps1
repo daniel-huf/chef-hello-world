2e69c380d8c85a7d04fa55bfa2ca5c14c32796cc
daniel-huf <daniel@scerus.com>
2021-10-06 10:41:51 +0200

    add testscript
    add step in pipleline to test if path still exists in new step

---
+++ pipeline/test-dir.ps1
@@ -0,0 +1,23 @@
+$cExists = Test-Path -Path C:\
+$cTmpExists = Test-Path -Path C:\Temp\
+
+if ($cExists) {
+    Write-Output "C:\ exists"
+}
+else {
+    Write-Output "C:\ doesn't exist"
+}
+if ($cTmpExists)) {
+    Write-Output "C:\Temp exists"
+}
+else {
+    Write-Output "C:\Temp doesn't exist"
+}
+Write-Output "Temp path is: $env:TEMP"
+
+Write-Output "==================="
+Write-Output "Create test directory"
+if($cTmpExists){
+    mkdir -Path "C:\Temp\test-dir"
+    Write-Output "Created dir: C:\Temp\test-dir"
+}
