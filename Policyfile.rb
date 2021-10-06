bf8936d1f99cfb699b83539b79eb19c06017633b
daniel-huf <daniel@scerus.com>
2021-10-06 16:50:52 +0200

    Add chef install step
    Creating policyfile.lock.json and using this to run chef

--- Policyfile.rb
+++ Policyfile.rb
@@ -10,7 +10,7 @@ name 'hello_world'
 default_source :supermarket
 
 # run_list: chef-client will run these recipes in the order specified.
-run_list 'hello_world::run'
+run_list 'hello_world::default'
 
 # Specify a custom source for a single cookbook:
 cookbook 'hello_world', path: '.'
