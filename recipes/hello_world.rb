37dd3aabf904545e9ef51eb175bbe9b4f9564008
daniel-huf <daniel@scerus.com>
2021-10-11 10:58:05 +0200

    Test commit trigger

--- recipes/hello_world.rb
+++ recipes/hello_world.rb
@@ -2,5 +2,5 @@ dsc_resource 'Hello-World-File-Exists' do
   resource :File
   property :Ensure, 'Present'
   property :DestinationPath, '$HOME\HelloWorld.txt'
-  property :Contents, 'Hello World!'
+  property :Contents, 'Hello World! This file was created by chef using DSC & Azure Pipelines'
 end
