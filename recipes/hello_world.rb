fdab04cd1c992d5921fe77d29c4bc349cf632d56
daniel-huf <daniel@scerus.com>
2021-10-11 10:52:34 +0200

    Change home dir variable

--- recipes/hello_world.rb
+++ recipes/hello_world.rb
@@ -1,6 +1,6 @@
 dsc_resource 'Hello-World-File-Exists' do
   resource :File
   property :Ensure, 'Present'
-  property :DestinationPath, "C:\Temp\HelloWorld.txt"
+  property :DestinationPath, '$HOME\HelloWorld.txt'
   property :Contents, 'Hello World!'
 end
